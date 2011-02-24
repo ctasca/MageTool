5c416b7b17269bcc1fd285f3221040090becf624
Alistair Stead <alistair.stead@designdisclosure.com>
2011-02-24 09:31:16 +0000

    added public accessors to properties and a __toString Method

--- MageTool/Lint/Message.php
+++ MageTool/Lint/Message.php
@@ -46,6 +46,39 @@ class MageTool_Lint_Message
     }
     
     /**
+     * Magic method invoked when the object is cast to a string
+     *
+     * @return void
+     * @author Alistair Stead
+     **/
+    public function __toString()
+    {
+        return $this->getLevel() . ': ' . $this->getMessage();
+    }
+    
+    /**
+     * Return the message level
+     *
+     * @return string
+     * @author Alistair Stead
+     **/
+    public function getLevel()
+    {
+        return $this->_message;
+    }
+    
+    /**
+     * Return the message string
+     *
+     * @return void
+     * @author Alistair Stead
+     **/
+    public function getMessage()
+    {
+        return $this->_message;
+    }
+    
+    /**
      * Write the message output to the response object
      *
      * @return void
@@ -54,8 +87,8 @@ class MageTool_Lint_Message
     public function write($response)
     {
         $response->appendContent(
-            $this->_level . ' :: ' . $this->_message,
-            array('color' => array($this->_colours[$this->_level]))
+            $this->getLevel() . ': ' . $this->getMessage(),
+            array('color' => array($this->_colours[$this->getLevel()]))
         );
     }
 }
