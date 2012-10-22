54c168bee690d04ba1e8f684a65ca595bf76f425
Alistair Stead <alistair.stead@designdisclosure.com>
2012-10-22 21:27:55 +0100

    Removed deprecated files from 0.1.* branch

--- bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Include Mage as a dependency for testing
-require_once '/Users/ali/Sites/magento.development.local/public/app/Mage.php';
-
-require_once 'Zend/Loader/Autoloader.php';
-require_once 'MageTool/Tool/Manifest.php';
-
-$manifest = new MageTool_Tool_Manifest();
